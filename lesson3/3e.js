798b03f85ca4cbf3d1fd5452867f3035e1924add
himanshugitafk <103594610+himanshugitafk@users.noreply.github.com>
2024-06-14 09:58:31 +0530

    Update 3e.js

--- lesson3/3e.js
+++ lesson3/3e.js
@@ -1,1 +1,2 @@
+// this is a property of Template strings called Interpolation where we can use ${} to do calculations or to use variables all without string concatenation and parenthesis use.
 `Total cost: $${5 + 3}`;
