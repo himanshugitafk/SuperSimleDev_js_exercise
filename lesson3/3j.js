21e62155e529dbc14dc6a47eec46a2b497d7cf0a
himanshugitafk <103594610+himanshugitafk@users.noreply.github.com>
2024-06-14 10:07:48 +0530

    Update 3j.js

--- lesson3/3j.js
+++ lesson3/3j.js
@@ -1,2 +1,5 @@
+// this is the 2nd property of template strings called multiline strings where we can use multiple lines to write a string the process to use them in console is  it to start with opening template string 
+// and type the string in multiple lines the closing template is to be used only when the string has been completed this is all to be taken care when writing in console.
+// in code editor we can write template strings the way we like as it will be interpreted only when we run the code.
 alert(`Total cost: $${(599+295)/100}
 Thank you, come again!`);
