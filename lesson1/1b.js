0eb6412159eaba1eb034f5fbcff195f1d2f54738
himanshugitafk <103594610+himanshugitafk@users.noreply.github.com>
2024-06-14 09:13:49 +0530

    Update 1b.js

--- lesson1/1b.js
+++ lesson1/1b.js
@@ -1,1 +1,4 @@
+// alert function is used to create a popup window displaying a specified message. 
+// It is provided by the window object.(which is the global object in the browser environment) That is why no explicit declaration required like window.alert('');
+// it can work simply by the way written below
 alert("Himanshu Mehta");
