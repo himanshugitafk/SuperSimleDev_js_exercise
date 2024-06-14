165e08b431f20f731ad6b87aa96299aee0a44a69
himanshugitafk <103594610+himanshugitafk@users.noreply.github.com>
2024-06-14 09:38:34 +0530

    Update 2f.js

--- lesson2/2f.js
+++ lesson2/2f.js
@@ -1,1 +1,2 @@
-(2095 + 799 + 1899) / 100
+// calcualting in $s can lead to floating point inaccuracies so convert $s to cents(1$ = 100cents) and then calculate the price in cents and then later to convert back to $ divide it by 100.
+(2095 + 799 + 1899) / 100;
