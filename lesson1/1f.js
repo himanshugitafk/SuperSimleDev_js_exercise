b35053bc74cfebd1d7b80b547b588e68653f05ee
himanshugitafk <103594610+himanshugitafk@users.noreply.github.com>
2024-06-14 09:25:14 +0530

    Update 1f.js

--- lesson1/1f.js
+++ lesson1/1f.js
@@ -1,1 +1,4 @@
+// document represents the Document Object Model (DOM) of the current web page. It's a global object that provides access to all the elements on the page.
+// body is a property of the document object that references the <body> element of the web page. The <body> element contains all the content that is displayed on the web page.
+// innerHTML is a property of the body (or any HTML element) that gets or sets the HTML markup contained within the element.
 document.body.innerHTML = "\'Good Morning!\'";
