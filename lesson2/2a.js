569f4b244338edea391ee218b7559069bcfb457f
himanshugitafk <103594610+himanshugitafk@users.noreply.github.com>
2024-06-14 09:31:32 +0530

    Update 2a.js

--- lesson2/2a.js
+++ lesson2/2a.js
@@ -1,1 +1,2 @@
-10 + 3 * 8 + 5
+// order of operations followed in js is P > E > MD > AS > comparison op. > logical op. > assign. op., if we have same precedence operators we calculate from Left to Right.
+10 + 3 * 8 + 5;
