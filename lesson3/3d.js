995dc3cc4226dc46d86c23f7f3e3c85947394c96
himanshugitafk <103594610+himanshugitafk@users.noreply.github.com>
2024-06-14 09:55:24 +0530

    Update 3d.js

--- lesson3/3d.js
+++ lesson3/3d.js
@@ -1,1 +1,2 @@
+// string concatenation done by + op. but to prioritize the calculation used parenthesis or else it would have done string concatenation of numbers too from Left to Right.
 'Total cost:$' + (5 + 3);
