d3fe85145b93c8d5de0b496217a3c26d8125fce0
himanshugitafk <103594610+himanshugitafk@users.noreply.github.com>
2024-06-14 09:43:09 +0530

    Update 2h.js

--- lesson2/2h.js
+++ lesson2/2h.js
@@ -1,1 +1,3 @@
+// Math is a built-in global object that has properties and methods for mathematical constants and functions.
+// round is a method of the Math object. It is used to round a number to the nearest integer.
 Math.round((2095 + 799 + (1899 + 499)) * 0.1) / 100 
